8c91a749a884d7bcb87223da6c43af69fef95984
Kannan Thambiah <pygospa@gmail.com>
2015-02-11 12:57:58 +0100

    Add zsh file for aliases and transfer all current aliases to that file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# File			~/.zsh/aliases.zsh
+#
+# Purpose		Aliases I like to use
+#			This file tests for certain executables, and if it
+#			finds them, aliases I like to use for these tools are
+#			created.
+#
+# Author		Kannan Thambiah <pygospa@gmail.com>
+# Lates Version		github.com/pygospa/dotfiles
+
+
+
+# --------------------------------------------------------------------------- #
+#			Version Controll Systems			      #
+# --------------------------------------------------------------------------- #
+
+if [[ -x `which git` ]]; then
+	alias g='git'
+fi
+
+
+
+
+
