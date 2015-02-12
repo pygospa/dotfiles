fdc3678e02fae1dcb170b91d9830578dbb093980
Kannan Thambiah <pygospa@gmail.com>
2015-02-12 23:11:16 +0100

    Add aliases for cd and du

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,7 +14,7 @@
 #			System						      #
 # --------------------------------------------------------------------------- #
 
-# LS aliases
+# ls aliases with and without color
 if [[ "$TERM" != dumb ]]; then
 	# -b = C-Style escapes
 	# -C = Columns
@@ -36,10 +36,41 @@ else
 	alias ls='ls -b -CF'
 fi
 
+# print current directory + subdirectories as a tree
+alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
+
+# six levels of shortcuts for cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+
+# aliases for du
+alias dh='du -hsc *'
+
+# Sort the files according to size
+if [[ `uname -s` == Linux ]]; then
+	alias dhs='du -hsc * | sort -h'
+# OS X doesn't know human-readable sort, so here's a workaround for that
+elif [[ `uname -s` == Darwin ]]; then
+	alias dhs='du -sckx * | sort -nr | awk '\''
+	function human(x) {
+		s="kMGTEPYZ";
+		while (x>=1000 && length(s)>1){
+			x/=1024; s=substr(s,2)
+		}
+		return (int(x*10)/10) substr(s,1,1)
+	}
+	{gsub(/^[0-9]+/, human($1)); print}'\'''
+fi
+
 # --------------------------------------------------------------------------- #
 #			Editor						      #
 # --------------------------------------------------------------------------- #
 
+# vi should point to vim
 if [[ -x `which vim` ]]; then
 	alias vi=`which vim`
 fi
@@ -48,6 +79,7 @@ fi
 #			Version Controll Systems			      #
 # --------------------------------------------------------------------------- #
 
+# I am a lazy bum
 if [[ -x `which git` ]]; then
 	alias g='git'
 fi
@@ -59,9 +91,9 @@ fi
 # On my Gentoo Linux run 'eisdrache' machine
 if [[ `uname -n` == eisdrache ]]; then
 	# Start mplayer with surround sound activated
-	alias surround='mplayer -ao alsa:device=plug=surround51 -channels 6'
+	alias m51='mplayer -ao alsa:device=plug=surround51 -channels 6'
 	# Start TV on mplayer with names for channels
-	alias fernseher='mplayer -tv driver=v4l2:chanlist=europe-west:channels=E6-ard,E8-zdf,E7-ndr,E10-sat1,SE18-rtl,E12-rtl2,E11-pro7,SE9-kabel1,SE10-vox,SE17-br,SE11-3sat,SE15-mdr,SE6-arte,SE14-n24,SE8-phoenix,SE7-mtv,SE12-viva,22-dasvierte,S22-dmax,S23-superrtl,SE16-kika,E9-kieltv,SE13-sport1,SE20-9live,S21-eurosport,S35-qvc,21-dr1:alsa:adevice=hw.1,0:amode=1:audiorate=32000:forceaudio:immediatemode=0:volume=100:norm=PAL:tdevice=/dev/vbi0 -vo gl2'
+	alias tv='mplayer -tv driver=v4l2:chanlist=europe-west:channels=E6-ard,E8-zdf,E7-ndr,E10-sat1,SE18-rtl,E12-rtl2,E11-pro7,SE9-kabel1,SE10-vox,SE17-br,SE11-3sat,SE15-mdr,SE6-arte,SE14-n24,SE8-phoenix,SE7-mtv,SE12-viva,22-dasvierte,S22-dmax,S23-superrtl,SE16-kika,E9-kieltv,SE13-sport1,SE20-9live,S21-eurosport,S35-qvc,21-dr1:alsa:adevice=hw.1,0:amode=1:audiorate=32000:forceaudio:immediatemode=0:volume=100:norm=PAL:tdevice=/dev/vbi0 -vo gl2'
 fi
 
 # On Macs in general
