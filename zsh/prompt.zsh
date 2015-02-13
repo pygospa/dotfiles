7f090c58c4bf71d817a0030d9f902bd79828a89f
Kannan Thambiah <pygospa@gmail.com>
2015-02-13 18:50:51 +0100

    Add prompt for OS X admin and root

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,7 @@
-PROMPT="${BLUE}%n${NO_COLOR}@%m %0~ %{$NO_COLOR%}%# "
+if (( EUID == 0 )) || [[ `whoami` == eru ]]; then 
+	PS1="${RED}%n${NO_COLOR}@%m %0d ## "
+else
+	PS1="${BLUE}%n${NO_COLOR}@%m %0~ %{$NO_COLOR%}%# "
+fi
+
 
