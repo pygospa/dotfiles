36b7e2a3230c1e22ccdc18cd6d74fa47a5895d56
Kannan Thambiah <pygospa@gmail.com>
2015-02-13 18:55:50 +0100

    Fix error producing double #

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,5 @@
 if (( EUID == 0 )) || [[ `whoami` == eru ]]; then 
-	PS1="${RED}%n${NO_COLOR}@%m %0d ## "
+	PS1="${RED}%n${NO_COLOR}@%m %0d %# "
 else
 	PS1="${BLUE}%n${NO_COLOR}@%m %0~ %{$NO_COLOR%}%# "
 fi
