73bea40359428f534b2cd80844abf9b4050f4675
Kannan Thambiah <pygospa@gmail.com>
2015-02-17 20:10:45 +0100

    Add aliases for Mac OS X

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -104,10 +104,16 @@ fi
 
 # On Macs in general
 if [[ `uname -s` == Darwin ]]; then
+
+	# Toggle Finder to show hidden files
+	alias show_hidden='defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder'
+	alias hide_hidden='defaults write com.apple.finder AppleShowAllFiles -boolean false ; killall Finder'
+
 	# Set easy to use commands for starting and stoping postgresql if
 	# available on system
 	if [[ -x `which pg_ctl` ]]; then
 		alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 		alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 	fi
+
 fi
