836ffa2b3731c3b22b35b86c984d281086ec4f94
Kannan Thambiah <pygospa@gmail.com>
2015-02-18 18:34:31 +0100

    Clean up files

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,11 +6,9 @@ precmd() {
 if (( EUID == 0 )) || [[ `whoami` == eru ]]; then 
 	PS1="${RED}%n${NO_COLOR}@%m %0d # "
 else
+	# Normal user
 	setopt prompt_subst
 	PS1='${BLUE}%n${NO_COLOR}@%m %0~ $(vcs_super_info) $(prompt_char) '
-#	PS1='${BLUE}%n${NO_COLOR}@%m %0~${vcs_info_msg_0_}%{$NO_COLOR%} $(prompt_char) '
-#	PS1='$(git_super_status)'
-#	PS1='$(vcs_super_info)'
 fi
 
 # Secondary prompt - printed when the shell needs more information to complete
