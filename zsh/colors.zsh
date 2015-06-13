de4240ba1aa15a57ef9bec0144b5e20965b0ce8e
Kannan Thambiah <pygospa@gmail.com>
2015-06-13 12:32:20 +0200

    Fix ANSI colors to meet zsh colors
    also add description to termcolors and ANSI color settings
    and move belonging parts (ls colors) together as a block

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,6 +1,8 @@
 # File			~/.zsh/colors.zsh
 #
 # Purpose		This file contains all color settings
+#			for the shell as well as for any shell command that is
+#			capable of producing colorful output
 #
 # Author		Kannan Thambiah <pygospa@gmail.com>
 # Latest Version	https://github.com/pygospa/dotfiles
@@ -24,16 +26,18 @@ if [[ "$TERM" != dumb ]]; then
 		WHITE="%{${fg[white]}%}"
 		NC="%{${reset_color}%}"
 
-		# Use ANSI escape codes
+	# Use ANSI escape codes
 	elif [[ "$TERM" != dumb ]]; then
-		print 'colors not available!'
-		BLUE=$'%{\e[1;34m%}'
-		RED=$'%{\e[1;31m%}'
-		GREEN=$'%{\e[1;32m%}'
-		CYAN=$'%{\e[1;36m%}'
-		WHITE=$'%{\e[1;37m%}'
-		MAGENTA=$'%{\e[1;35m%}'
+		print 'ZSH module colors not available! Switching to ANSI colors'
 		YELLOW=$'%{\e[1;33m%}'
+		ORANGE=$'%{\e[0;33m%}'		# brownish color
+		RED=$'%{\e[1;31m%}'
+		MAGENTA=$'%{\e[1;35m%}'
+		VIOLET=$'%{\e[0;35m%}'		# dark magenta
+		BLUE=$'%{\e[1;34m%}'
+		CYAN=$'%{\e[1;36m%}'
+		GREEN=$'%{\e[1;32m%}'
+		WHITE=$'%{\e[1;37m%}'
 		NO_COLOR=$'%{\e[0m%}'
 	fi
 
@@ -44,14 +48,6 @@ if [[ "$TERM" != dumb ]]; then
 		export COLORTERM="yes"
 	fi
 
-	# Colors for vi
-	typeset -ga ls_options
-	if ls --help 2> /dev/null | grep -q GNU; then
-		ls_options=( --color=auto )
-	elif [[ `uname -s` == FreeBSD ]]; then
-		ls_options=( -G )
-	fi
-
 	# Colors for grep
 	typeset -ga grep_options
 	if grep --help 2> /dev/null | grep -q GNU || [[ `uname -s` == FreeBSD ]]; then
@@ -59,6 +55,13 @@ if [[ "$TERM" != dumb ]]; then
 	fi
 
 	# Colors for ls
+	typeset -ga ls_options
+	if ls --help 2> /dev/null | grep -q GNU; then
+		ls_options=( --color=auto )
+	elif [[ `uname -s` == FreeBSD ]]; then
+		ls_options=( -G )
+	fi
+
 	# (dircolors will populate LS_COLORS)
 	if [[ -x `which dircolors` ]]; then
 		eval $(dircolors -b)
@@ -66,15 +69,16 @@ if [[ "$TERM" != dumb ]]; then
 
 	# Set colors in less
 	if [[ -x `which less` ]]; then
-		export LESS_TERMCAP_mb=$'\E[01;32m'
-		export LESS_TERMCAP_md=$'\E[01;36m'
-		export LESS_TERMCAP_me=$'\E[0m'
-		export LESS_TERMCAP_se=$'\E[0m'
-		export LESS_TERMCAP_so=$'\E[01;33m'
-		export LESS_TERMCAP_ue=$'\E[0m'
-		export LESS_TERMCAP_us=$'\E[01;35m'
+		export LESS_TERMCAP_mb=$'\E[01;32m'	# green		mark blinking
+		export LESS_TERMCAP_md=$'\E[01;36m'	# cyan		mark bold
+		export LESS_TERMCAP_me=$'\E[0m'		# no color	mark end (of blinking bold)
+		export LESS_TERMCAP_so=$'\E[01;33m'	# yellow	start standout
+		export LESS_TERMCAP_se=$'\E[0m'		# no color	stop standout
+		export LESS_TERMCAP_us=$'\E[01;35m'	# magenta	start underline
+		export LESS_TERMCAP_ue=$'\E[0m'		# no color	end underline
 	fi
-# No color terminal
+	
+# Dull terminal (i.e. no colors)
 else
 	if [[ `uname -s` == Darwin || `uname -s` == FreeBSD ]]; then
 		export CLITERM=0
