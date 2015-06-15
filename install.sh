323620f72f35c70d8ce1b0817d958d0a16b2c166
Kannan Thambiah <pygospa@gmail.com>
2015-06-15 23:58:48 +0200

    Improve install scripts
    Install script now backups all dotfiles from home before installing
    files from dir
    
    Deinstall removes all links to dotfile repository and restores
    dotfiles in backup file

--- install.sh
+++ install.sh
@@ -4,10 +4,17 @@
 
 # Dotfilepath and files to exclude
 DFP=$( (cd -P $(dirname $0) && pwd) )
-EXCL=(install.sh ousted README.md)
+EXCL=(backup install.sh ousted README.md)
 
+# Create backup directory inside dotfilepath
+mkdir $DFP/backup
+
+# Create symbolic link to configs and save pre-existing dotfiles into DFP/backup
 for F in *; do 
   if ! [[ ${EXCL[*]} =~ "$F" ]]; then
+    if [[ -f ~/.$F ]]; then
+      mv ~/.$F $DFP/backup/.$F
+    fi
     ln -sf $DFP/$F ~/.$F
   fi
 done
