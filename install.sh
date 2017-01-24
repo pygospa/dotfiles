e5cd3bdfb489a14b401ed4d80a68da70431f8724
Kannan Thambiah <pygospa@gmail.com>
2017-01-24 12:23:52 +0100

    Tweak install.sh to only create dirs if not excluded

--- install.sh
+++ install.sh
@@ -6,36 +6,64 @@
 # Latest version: https://github.com/pygospa/dotfiles
 
 
+#------------------------------------------------------------------------------
+# Variable setup
+#
+
 # Dotfilepath and files to exclude
 DFP=$( (cd -P $(dirname $0) && pwd) )
 
 # If we are on a macOS powered machine, exclude typical Linux only software
 if [[ `uname -s` == Darwin ]]; then
-  EXCL=( asound backup config/user-dirs.dirs install.sh LICENSE mutt muttrc\
-	  ousted README.md uninstall.sh xinitrc Xresources )
+  EXCL=( asound backup config/user-dirs.dirs i3 install.sh LICENSE mutt muttrc\
+	  ousted README.md uninstall.sh xinitrc Xresources zsh )
 # If we are on a non macOS powered Unix machine, exclude only the repository stuff
 else
   EXCL=( backup install.sh LICENSE ousted README.md uninstall.sh )
 fi
 
+
+#------------------------------------------------------------------------------
+# Directory creation
+#
+
 # Create a backup directory inside the dotfile path
 mkdir -p $DFP/backup
 
-# Copy pre-existing dotfiles that would be overwritten into $DFP/backup
-# then symlink all the dotfiles
+# Do we need directories for the Z-Shell?
+if ! [[ ${EXCL[*]} =~ "zsh" ]]; then
+	mkdir -p $DFP/zsh/zkbd
+fi
+
+# Do we need directories for vim?
+if ! [[ ${EXCL[*]} =~ "vim" ]]; then
+	mkdir -p $HOME/tmp/vim/swaps
+	mkdir -p $HOME/tmp/vim/backups
+	mkdir -p $HOME/tmp/vim/undos
+fi
+
+# Do we need directories for i3?
+if ! [[ ${EXCL[*]} =~ "vim" ]]; then
+	mkdir -p $HOME/tmp/shots
+fi
+
+
+#------------------------------------------------------------------------------
+# Install actual config files
+#
+
 for file in *; do
+  # File is not excluded
   if ! [[ ${EXCL[*]} =~ "$file" ]]; then
+    # config already exist? Move it to $DFP/backup and be verbose about it
     if [[ -f ~/.$file || -d ~/.$file ]]; then
       echo ""
       echo "mv ~/.$file $DFP/backup/$file"
       mv ~/.$file $DFP/backup/$file
     fi
+    # link the config file from $DSP to $HOME as expected by application
     echo "ln -sf $DFP/$file ~/.$file"
     ln -sf $DFP/$file ~/.$file
   fi
 done
 
-# Create the vimfiles directory for vim
-mkdir -p $HOME/tmp/vim/swaps
-mkdir -p $HOME/tmp/vim/backups
-mkdir -p $HOME/tmp/vim/undos
