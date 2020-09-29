9ae50925ce4fdbd82191b6f3d6e68abc7357b1cc
Kannan Thambiah <pygospa@gmail.com>
2020-09-29 22:45:11 +0200

    [ansible] Add Python, pip & pythonenv

--- zsh/envvars.zsh
+++ zsh/envvars.zsh
@@ -53,6 +53,13 @@ fi
 if [[ -d "$HOME/.rvm/bin" ]]; then
 	export PATH="$PATH:$HOME/.rvm/bin"
 	[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
 fi
 
+# Python envrionments
+if [[ -s "/usr/local/bin/virtualenvwrapper.sh" ]]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=' -p /usr/bin/python3 '
+    export PROJECT_HOME=$HOME/files/slab/python
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
